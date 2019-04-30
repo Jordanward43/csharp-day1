85f9aa85f4c4a75cd9eb6fc063f425d90d1e23b4
Jordanward43 <fallbrook43@gmail.com>
2019-04-29 19:34:18 -0600

    initialize

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello User, what is your Name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+             //Console.WriteLine("Hello" + name + ". It sounds like "+ "is" + age + "years old");
+             //Console.WriteLine($"Hello {name}. It sounds like {name} is {age} years old.");
+             Console.WriteLine("Hello {0}. It sounds like {0} is {1} years old.", name, age);
+
+             int myNum = 25;
+             myNum = myNum * 2;
+             myNum *= 2;
+
+
+             Console.WriteLine(15 % 12);
+
+             bool isAwesome = true;
+
+
+        }
+    }
+}
